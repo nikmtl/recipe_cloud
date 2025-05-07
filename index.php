582d7f1c7306f9b2b4ff7be6df53076e9eb983d9
Nik <127861651+NikMitzel@users.noreply.github.com>
2025-05-07 19:02:02 +0200

    feat: add footer section with links and logo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- Load stylesheets -->
     <link rel="stylesheet" href="assets/css/style.css">
 
-    <!-- Load Inter font from Google Fonts --> <!--TODO: change to own font loading because of EU law-->
+    <!-- Load Inter font from Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
@@ -31,7 +31,7 @@
                 <a href="upload.php">Upload</a>
             </div>
             <div class="auth-buttons">
-                <button class="gost-button icon-button">
+                <button class="gost-button icon-button" onclick="location.href='recipes.php'">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search h-5 w-5">
                         <circle cx="11" cy="11" r="8"></circle>
                         <path d="m21 21-4.3-4.3"></path>
@@ -48,8 +48,44 @@
             </div>
         </div>
     </header>
-
-
+    <main>
+        
+    </main>
+    <footer>
+        <div>
+            <div class="footer-logo-container">
+                <div class="footer-logo">
+                    <img src="assets/img/logo.svg" alt="Recipe Cloud Logo" class="logo" width="24" height="24">
+                    <h1>Recipe Cloud</h1>
+                </div>
+                <p>Share and discover delicious recipes from around the world.</p>
+            </div>
+            <div class="footer-links-container">
+                <h2>Quick Links</h2>
+                <div>
+                    <a href="">Home</a>
+                    <a href="recipes.php">Recipes</a>
+                    <a href="upload.php">Upload</a>
+                </div>
+            </div>
+            <div class="footer-links-container">
+                <h2>Legal (available if site is live)</h2>
+                <div>
+                    <a href="">Privacy Policy</a>
+                    <a href="">Terms of Service</a>
+                    <a href="">Cookie Preferences</a>
+                    <a href="">Contact Us</a>
+                </div>
+            </div>
+            <div class="footer-links-container">
+                <h2>Connect</h2>
+                <div>
+                    <a href="https://github.com/Edamame04/recipe_cloud">GitHub</a>
+                    <a href="https://github.com/Edamame04/recipe_cloud/issues">Report a Bug</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
