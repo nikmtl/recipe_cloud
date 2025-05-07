4a78cb5202231197b35f06eac26d158cd352500b
Nik <127861651+NikMitzel@users.noreply.github.com>
2025-05-07 13:41:45 +0200

    feat: add initial HTML structure, styles, and logo assets

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="src/img/logo_with_bg.svg" type="image/svg+xml">
+    <meta name="description" content="Recipe Cloud - Your go-to place for delicious recipes.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recipe Cloud</title>
+
+    <!-- Load stylesheets -->
+    <link rel="stylesheet" href="src/css/style.css">
+
+    <!-- Load Inter font from Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div>
+            <div class="logo-container">
+                <img src="src/img/logo.svg" alt="Recipe Cloud Logo" class="logo" width="24" height="24">
+                <h1>Recipe Cloud</h1>
+            </div>
+            <div class="nav-links">
+                <a href="index.php">Home</a>
+                <a href="recipes.php">Recipes</a>
+                <a href="upload.php">Upload</a>
+            </div>
+            <div class="auth-buttons">
+                <button class="secondary-button">Sign In</button>
+                <button>Register</button>
+            </div>
+        </div>
+    </header>
+
+
+</body>
+
+</html>
