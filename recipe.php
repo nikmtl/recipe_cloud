b892cf462dae57c730960bdffb269cef47d3f53a
Nik <127861651+NikMitzel@users.noreply.github.com>
2025-06-21 15:00:24 +0200

    fix: add conditional rendering for recipe image in header

--- recipe.php
+++ recipe.php
@@ -97,11 +97,13 @@ include_once 'assets/includes/header.php'; //load header
         <a class="back-button" href="recipes.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left mr-2 h-4 w-4" __v0_r="0,3543,3557">
                 <path d="m12 19-7-7 7-7"></path>
                 <path d="M19 12H5"></path>
-            </svg>Back to Recipes</a>
-        <div>
+            </svg>Back to Recipes</a>        
+            <div>
+            <?php if (!empty($recipe['image_path'])): ?>
             <div class="recipe-image-header">
                 <img src="<?php echo htmlspecialchars($recipe['image_path']); ?>" alt="<?php echo htmlspecialchars($recipe['title']); ?>" class="recipe-image">
             </div>
+            <?php endif; ?>
             <div class="recipe-content">
                 <div class="recipe-details">
                     <div class="recipe-header">
