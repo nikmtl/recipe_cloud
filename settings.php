cb5b3bf321127608c348e64864c1db62d547eb12
Edamame04 <nik.mitzel@gmail.com>
2025-07-17 13:42:25 +0200

    fix: update button label for password change form

--- settings.php
+++ settings.php
@@ -81,11 +81,9 @@ if (!$userProfile) {
                 </div>
                 <button type="submit" class="icon-button smt-bttn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path>
-                        <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path>
-                        <path d="M7 3v4a1 1 0 0 0 1 1h7"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
                     </svg>
-                    Save Changes
+                    Update Password
                 </button>
             </form>
         </div>
